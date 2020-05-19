55b38ab4aedd08d7be52399318e087ec438661ba
yangyuhang700 <905185417@qq.com>
2020-05-19 08:55:14 +0800

    5-18

---
+++ 5.18作业/03作业/pages/demo3/demo3.js
@@ -0,0 +1,79 @@
+
+// pages/demo3/demo3.js
+Page({
+data:{
+  obj11:[
+    {text:"刘德华"},
+    {text:"AnyLau,Lau Tak Wah"},
+    {text:"华仔,华Dee,华哥等"},
+    {text:"中国"},
+    {text:"汉族"},
+    {text:"天秤座"},
+    {text:"AB型"},
+    {text:"174cm"},
+    {text:"63kg"},
+    {text:"香港新界大埔镇泰亨村"},
+    {text:"1961年9月27日"},
+  ]
+},
+  /**
+   * 页面的初始数据
+   */
+ 
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
